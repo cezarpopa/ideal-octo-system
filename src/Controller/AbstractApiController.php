25a67c3ee563f084d633ec5893e082c86b38b806
Cezar Popa <cezarpopa@users.noreply.github.com>
2022-02-10 21:39:15 +0000

    Remove unnecessary use statement

--- src/Controller/AbstractApiController.php
+++ src/Controller/AbstractApiController.php
@@ -7,7 +7,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 abstract class AbstractApiController extends AbstractController
